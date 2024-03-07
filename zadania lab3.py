531b10f23c6e061894b41179734807afa68191f8
Mateusz1811 <147046153+Mateusz1811@users.noreply.github.com>
2024-03-07 13:17:46 +0100

    zadania

--- zadania lab3.py
+++ zadania lab3.py
@@ -5,6 +5,7 @@
 #    print(result)
 # except ZeroDivisionError:
 #     print("Error")
+import math
 
 a = [x**2 for x in range(10)]
 print(a)
@@ -29,3 +30,30 @@ print(l3)
 s1 = {1:2, 2:3, 3:4}
 s2 = {v: k for k, v in s1.items()}
 print(s2)
+
+# def rownanie_kwadratowe(a, b, c):
+#     delta = b**2 -4*a*c
+#     if delta < 0:
+#         print("brak pierwiastka")
+#         return 0
+#     elif delta == 0:
+#         print("Jeden pierwiastek")
+#         x = -b/2*a
+#         return x
+#     elif delta > 0:
+#         print("dwa rozwiazania")
+#         x1 = (-b - math.sqrt(delta))/(2*a))
+#         x2 = (-b + math.sqrt(delta))/(2*a))
+#         return x1, x2
+# print(rownanie_kwadratowe(6,1,3))
+# print(rownanie_kwadratowe(1,2,1))
+# print(rownanie_kwadratowe(1,4,1))\
+
+def dlugosc_odcinka(x1=1, x2=2, y1=3, y2=4):
+    return math.sqrt((x2-x1))**2 + (y2-y1)**2
+
+print(dlugosc_odcinka())
+print(dlugosc_odcinka(2,4,5,7))
+print(dlugosc_odcinka(1,2,3,4))
+print(dlugosc_odcinka(2,3,7,8))
+
